a5971886e9af54af042ec280c1b60d726da91bd4
YuhriParada <yuhriparada@gmail.com>
2021-08-18 01:03:48 -0300

    app working with some style

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_152846) do
+ActiveRecord::Schema.define(version: 2021_08_17_185953) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.integer "details"
+    t.text "details"
     t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
